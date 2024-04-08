abb5a0d4f858c482778db0a8d7c4ebba97c25c2b
supredu <1020665094@qq.com>
2024-04-08 21:09:28 +0800

    add test 1

--- duUSD/src/factory/PoolFactory.sol
+++ duUSD/src/factory/PoolFactory.sol
@@ -1,122 +1,122 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Address.sol";
+// import "@openzeppelin/contracts/proxy/Clones.sol";
 
-interface IAddressProvider {
-    function admin() external view returns (address);
-    function getRegistry() external view returns (address);
-}
+// interface IAddressProvider {
+//     function admin() external view returns (address);
+//     function getRegistry() external view returns (address);
+// }
 
-interface IRegistry {
-    function getLpToken(address pool) external view returns (address);
-    function getNCoins(address pool) external view returns (uint256);
-    function getCoins(address pool) external view returns (address[] memory);
-    function getPoolFromLpToken(address lpToken) external view returns (address);
-}
+// interface IRegistry {
+//     function getLpToken(address pool) external view returns (address);
+//     function getNCoins(address pool) external view returns (uint256);
+//     function getCoins(address pool) external view returns (address[] memory);
+//     function getPoolFromLpToken(address lpToken) external view returns (address);
+// }
 
-interface IERC20Extended is IERC20 {
-    function decimals() external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-}
+// interface IERC20Extended is IERC20 {
+//     function decimals() external view returns (uint256);
+//     function approve(address spender, uint256 amount) external returns (bool);
+// }
 
-interface ICurvePlainPool {
-    function initialize(
-        string memory name,
-        string memory symbol,
-        address[] memory coins,
-        uint256[] memory rateMultipliers,
-        uint256 A,
-        uint256 fee
-    ) external;
-}
+// interface ICurvePlainPool {
+//     function initialize(
+//         string memory name,
+//         string memory symbol,
+//         address[] memory coins,
+//         uint256[] memory rateMultipliers,
+//         uint256 A,
+//         uint256 fee
+//     ) external;
+// }
 
-interface ICurvePool {
-    function A() external view returns (uint256);
-    function fee() external view returns (uint256);
-    function adminFee() external view returns (uint256);
-    function balances(uint256 i) external view returns (uint256);
-    function adminBalances(uint256 i) external view returns (uint256);
-    function getVirtualPrice() external view returns (uint256);
-    function initialize(
-        string memory name,
-        string memory symbol,
-        address coin,
-        uint256 rateMultiplier,
-        uint256 A,
-        uint256 fee
-    ) external;
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 minDy,
-        address receiver
-    ) external returns (uint256);
-}
+// interface ICurvePool {
+//     function A() external view returns (uint256);
+//     function fee() external view returns (uint256);
+//     function adminFee() external view returns (uint256);
+//     function balances(uint256 i) external view returns (uint256);
+//     function adminBalances(uint256 i) external view returns (uint256);
+//     function getVirtualPrice() external view returns (uint256);
+//     function initialize(
+//         string memory name,
+//         string memory symbol,
+//         address coin,
+//         uint256 rateMultiplier,
+//         uint256 A,
+//         uint256 fee
+//     ) external;
+//     function exchange(
+//         int128 i,
+//         int128 j,
+//         uint256 dx,
+//         uint256 minDy,
+//         address receiver
+//     ) external returns (uint256);
+// }
 
-interface ICurveFactoryMetapool {
-    function coins(uint256 i) external view returns (address);
-    function decimals() external view returns (uint256);
-}
+// interface ICurveFactoryMetapool {
+//     function coins(uint256 i) external view returns (address);
+//     function decimals() external view returns (uint256);
+// }
 
-interface ILiquidityGauge {
-    function initialize(address lpToken) external;
-}
+// interface ILiquidityGauge {
+//     function initialize(address lpToken) external;
+// }
 
-contract CurveFactory is Ownable {
-    using Clones for address;
-    using Address for address;
+// contract CurveFactory is Ownable {
+//     using Clones for address;
+//     using Address for address;
 
-    // Define events
-    event BasePoolAdded(address indexed basePool);
-    event PlainPoolDeployed(address[] coins, uint256 A, uint256 fee, address indexed deployer, address pool);
-    event MetaPoolDeployed(address coin, address basePool, uint256 A, uint256 fee, address indexed deployer);
-    event LiquidityGaugeDeployed(address pool, address gauge);
+//     // Define events
+//     event BasePoolAdded(address indexed basePool);
+//     event PlainPoolDeployed(address[] coins, uint256 A, uint256 fee, address indexed deployer, address pool);
+//     event MetaPoolDeployed(address coin, address basePool, uint256 A, uint256 fee, address indexed deployer);
+//     event LiquidityGaugeDeployed(address pool, address gauge);
 
-    // Define the maximum number of coins and plain coins
-    uint256 private constant MAX_COINS = 8;
-    uint256 private constant MAX_PLAIN_COINS = 4;
+//     // Define the maximum number of coins and plain coins
+//     uint256 private constant MAX_COINS = 8;
+//     uint256 private constant MAX_PLAIN_COINS = 4;
 
-    // Define the address provider and old factory addresses
-    address private constant ADDRESS_PROVIDER = 0x0000000022D53366457F9d5E68Ec105046FC4383;
-    address private constant OLD_FACTORY = 0x0959158b6040D32d04c301A72CBFD6b39E21c9AE;
+//     // Define the address provider and old factory addresses
+//     address private constant ADDRESS_PROVIDER = 0x0000000022D53366457F9d5E68Ec105046FC4383;
+//     address private constant OLD_FACTORY = 0x0959158b6040D32d04c301A72CBFD6b39E21c9AE;
 
-    // State variables
-    address public futureAdmin;
-    address public manager;
+//     // State variables
+//     address public futureAdmin;
+//     address public manager;
 
-    address[] public poolList;  // Master list of pools
-    uint256 public poolCount;  // Actual length of poolList
+//     address[] public poolList;  // Master list of pools
+//     uint256 public poolCount;  // Actual length of poolList
 
-    // Define other state variables and mappings here
+//     // Define other state variables and mappings here
 
-    constructor(address _feeReceiver) Ownable() {
-        // Constructor logic here
-    }
+//     constructor(address _feeReceiver) Ownable() {
+//         // Constructor logic here
+//     }
 
-    // Define functions here
+//     // Define functions here
 
-    // Example function to deploy a plain pool
-    function deployPlainPool(
-        string memory _name,
-        string memory _symbol,
-        address[] memory _coins,
-        uint256 _A,
-        uint256 _fee,
-        uint256 _assetType,
-        uint256 _implementationIdx
-    ) external onlyOwner returns (address) {
-        // Deployment logic here
+//     // Example function to deploy a plain pool
+//     function deployPlainPool(
+//         string memory _name,
+//         string memory _symbol,
+//         address[] memory _coins,
+//         uint256 _A,
+//         uint256 _fee,
+//         uint256 _assetType,
+//         uint256 _implementationIdx
+//     ) external onlyOwner returns (address) {
+//         // Deployment logic here
 
-        emit PlainPoolDeployed(_coins, _A, _fee, msg.sender, address(0));  // Example event
-        return address(0);  // Placeholder return
-    }
+//         emit PlainPoolDeployed(_coins, _A, _fee, msg.sender, address(0));  // Example event
+//         return address(0);  // Placeholder return
+//     }
 
-    // Other functions based on the Vyper contract's methods need to be implemented...
+//     // Other functions based on the Vyper contract's methods need to be implemented...
 
-    // Note: This contract is a simplified template and requires detailed implementation based on the Vyper contract's logic.
-}
+//     // Note: This contract is a simplified template and requires detailed implementation based on the Vyper contract's logic.
+// }
